c97c32c69566f6d774ff2015d967cbc5f2340536
Ambiente 209-2 <Ambiente 209-2@NEIDFPSITFSD257.FormacionSena.RED>
2023-08-13 13:10:32 -0500

    cambios

---
+++ Evidencia/src/Class/ClasePadre.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+import Utils.FuntionNumeric;
+import Utils.FuntionString;
+/**
+ *
+ * @author Ambiente 209-2
+ */
+public class ClasePadre {
+   private Double promedio;
+   
+   public void setPromedio (Double promedio){
+       this.promedio = promedio;
+   }
+   
+   public Double getPromedio (){
+       return promedio;
+   }
+   
+   public void total(){
+       FuntionNumeric fn = new FuntionNumeric();
+       
+   }
+    
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Evidencia/src/view/muestra.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+import java.util.Scanner;
+import Class.ClasePadre;
+/**
+ *
+ * @author Ambiente 209-2
+ */
+public class muestra {
+    public static void main(String[] args) {
+
+    }
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
